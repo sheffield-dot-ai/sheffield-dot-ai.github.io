c099887edc7e411e559a38e946f76711acecfec2
James Charlesworth <jcharlesworthuk@gmail.com>
2023-06-17 13:14:34 +0100

    Add content to the home page banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,22 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import sheffieldLandscapeImage from "../images/sheffield-landscape.jpg";
 import robotAndCityImage from "../images/robot-and-city.svg";
+import sheffieldAiTextImage from "../images/text-only.svg";
+import eventBriteLogoImage from "../images/eventbrite-logo.svg";
 import * as classes from "./index.module.css";
+
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className={classes.main} style={{ backgroundImage: `url(${sheffieldLandscapeImage})` }}>
       <section className={classes.banner}>
-        <h1>Sheffield AI</h1>
+        <div className={classes.bannerContents}>
+          <h1><img className={classes.textOnly} src={sheffieldAiTextImage} alt="sheffield.ai" /></h1>
+          <h2 className={classes.titleText}>Events for humans</h2>
+          <p>Sign up for the newsletter to find out about upcoming events and more</p>
+          <div></div>
+          <p>Find us on <a href="#"><img src={eventBriteLogoImage} alt="Eventbrite logo" /></a></p>
+        </div>
         <img className={classes.robotAndCity} src={robotAndCityImage} alt="Image of robot" />
       </section>
 
