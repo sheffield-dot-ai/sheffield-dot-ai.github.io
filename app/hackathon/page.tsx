d26132b496ca094ff05163d9bd9118cac02121f7
James Charlesworth <jcharlesworthuk@gmail.com>
2024-12-08 23:04:24 +0000

    Added hackathon page stub

---
+++ app/hackathon/page.tsx
@@ -0,0 +1,12 @@
+import classes from "./page.module.css";
+import SiteFooter from "@/components/global/SiteFooter";
+
+export default function IndexPage() {
+  return <main className={classes.main}>
+        <h2 className={classes.title1}>Sheffield</h2>
+        <h1 className={classes.title2}>AI • Hackathon</h1>
+        <h3 className={classes.title3}>May 2025</h3>
+    <SiteFooter className={classes.footer} />
+
+  </main>
+}
