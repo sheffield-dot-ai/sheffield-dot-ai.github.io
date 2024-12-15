c21fe8399ebe0c056c28e3cbb6021b962ead4ab2
James Charlesworth <jcharlesworthuk@gmail.com>
2024-12-15 11:36:18 +0000

    Hackathon landing page started

--- app/hackathon/page.tsx
+++ app/hackathon/page.tsx
@@ -5,7 +5,9 @@ export default function IndexPage() {
   return <main className={classes.main}>
         <h2 className={classes.title1}>Sheffield</h2>
         <h1 className={classes.title2}>AI • Hackathon</h1>
-        <h3 className={classes.title3}>May 2025</h3>
+        <h3 className={classes.title3}>May 2025
+          <img src="/img/robot-and-city.svg" className={classes.robotImage } />
+        </h3>
     <SiteFooter className={classes.footer} />
 
   </main>
