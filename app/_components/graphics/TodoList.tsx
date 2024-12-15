e83b9928685d911b5319993cb672e437d2ef90d3
James Charlesworth <jcharlesworthuk@gmail.com>
2024-12-15 21:25:55 +0000

    Added a basic page layout to the hackathon landing page

---
+++ app/_components/graphics/TodoList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './TodoList.module.css';
+
+type Props = React.PropsWithChildren<{
+    className?: string,
+    title: string
+}>
+
+export default function TodoList(props: Props) {
+
+    return <div className={[props.className ?? '', classes.container].join(' ')}>
+        <h4 className={classes.title}>{props.title}</h4>
+        <svg viewBox="0 0 400 20" xmlns="http://www.w3.org/2000/svg" className={classes.underline} style={{width: `${(props.title.length / 2) - 1}em`}}>
+            <path id="svg_line" d="m 1.986,8.91 c 55.429038,4.081 111.58111,5.822 167.11781,2.867 22.70911,-1.208 45.39828,-0.601 68.126,-0.778 28.38173,-0.223 56.76079,-1.024 85.13721,-1.33 24.17379,-0.261 48.42731,0.571 72.58115,0.571"></path>
+        </svg>
+        <ul className={classes.list}>
+            {props.children}
+        </ul>
+    </div>
+}
--- app/hackathon/page.tsx
+++ app/hackathon/page.tsx
@@ -1,14 +1,63 @@
+import TodoList from "@/components/graphics/TodoList";
 import classes from "./page.module.css";
 import SiteFooter from "@/components/global/SiteFooter";
 
 export default function IndexPage() {
   return <main className={classes.main}>
-        <h2 className={classes.title1}>Sheffield</h2>
-        <h1 className={classes.title2}>AI • Hackathon</h1>
-        <h3 className={classes.title3}>May 2025
-          <img src="/img/robot-and-city.svg" className={classes.robotImage } />
-        </h3>
-    <SiteFooter className={classes.footer} />
+    <h2 className={classes.title1}>Sheffield</h2>
+    <h1 className={classes.title2}>AI • Hackathon</h1>
+    <h3 className={classes.title3}>May 2025
+      <img src="/img/robot-and-city.svg" className={classes.robotImage} />
+    </h3>
+    <section className={[classes.container, classes.contrastBlock].join(' ')}>
+      <div className={classes.flexRow}>
+        <div className={classes.todoListWrapper}>
+          <TodoList className={classes.todoList} title="Hackathon 2025">
+            <li>Save the date (TBC)</li>
+            <li>Read the rules</li>
+            <li>Join a team</li>
+            <li>Build a project</li>
+            <li>Win a prize</li>
+          </TodoList>
+        </div>
+        <div>
+          <h2>Calling AI Makers</h2>
+          <p>Sheffield AI are running a FREE AI based hackathon in May 2025.  Entries are open to anybody in the region who wants to get hands on and build something with AI.  More details to follow closer to the time.</p>
+        </div>
+      </div>
+    </section>
+    <section className={[classes.container, classes.agenda].join(' ')}>
+      <h2>Draft Agenda</h2>
+        <h3>Day 1: [Date]</h3>
+      <ul>
+        <li>
+          <time dateTime="09:00">9:00 AM</time>
+          <span>Kickoff</span>
+          <address>Vanue</address>
+        </li>
+        <li>
+          <time dateTime="09:30">09:30 AM</time>
+          <span>Project Presentations</span>
+          <address>Venue</address>
+        </li>
+        <li>
+          <time dateTime="11:00">11:00 AM</time>
+          <span>Hacking Begins</span>
+          <address>Venue</address>
+        </li>
+      </ul>
+        <h3>Day 2: [Date]</h3>
+      <ul>
+        <li>
+          <time dateTime="12:00">12:00 PM</time>
+          <span>Project Deadline</span>
+          <address>Online</address>
+        </li>
 
+      </ul>
+    </section>
+    <section className={classes.centreFlex}>
+      <SiteFooter className={classes.footer} />
+    </section>
   </main>
 }
